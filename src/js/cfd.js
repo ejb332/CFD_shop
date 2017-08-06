6b5496ec9277c039641b205e3ab44e7aa8503ae4
Eric Biel <eric.j.biel@gmail.com>
2017-08-06 16:22:17 -0500

    Add hover effects and styling to main buttons

---
+++ src/js/cfd.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  // a-series hover effect
+  $('#a').mouseover(function() {;
+    $('#a-drop').slideToggle(1500);
+  });
+
+  $('#avf').mouseover(function() {;
+    $('#avf-drop').slideToggle(1500);
+  });
+
+  // b-series hover effect
+  $('#b').mouseover(function() {;
+    $('#b-drop').slideToggle(1500);
+  });
+
+  $('#c').mouseover(function() {;
+    $('#c-drop').slideToggle(1500);
+  });
+
+  $('#d').mouseover(function() {;
+    $('#d-drop').slideToggle(1500);
+  });
+
+  $('#e').mouseover(function() {;
+    $('#e-drop').slideToggle(1500);
+  });
+
+  $('#g').mouseover(function() {;
+    $('#g-drop').slideToggle(1500);
+  });
+
+  $('#s').mouseover(function() {;
+    $('#s-drop').slideToggle(1500);
+  });
+
+  $('#former').mouseover(function() {;
+    $('#former-drop').slideToggle(1500);
+  });
+
+  $('#unid').mouseover(function() {;
+    $('#unid-drop').slideToggle(1500);
+  });
+
+
+});
